69cc78e8f0b056379a345b84d0210fd252bca1d5
Shreeja Tirumalasetty <ShreejaTirumalasetty.gmail.com>
2025-02-05 12:44:20 +0530

    frontend

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('predictButton').addEventListener('click', function() {
+    alert('Predicting market sentiment... Please wait!');
+    // You can add more functionality here, like fetching data from an API
+});
