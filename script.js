ed6fdee8c767ec066463e034ff41907b42b1e72a
Shreeja Tirumalasetty <ShreejaTirumalasetty.gmail.com>
2025-02-07 12:54:02 +0530

    predict

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('predictButton').addEventListener('click', function() {
-    alert('Predicting market sentiment... Please wait!');
-    // You can add more functionality here, like fetching data from an API
-});
